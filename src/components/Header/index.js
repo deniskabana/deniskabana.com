e476fba48d32d984a56791800ab6b55ef41a1dfc
Denis Kabana <denis.kabana@gmail.com>
2018-04-01 00:16:14 +0200

    Setup gatsby project

---
+++ src/components/Header/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const StyledHeaderBg = styled.div`
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background: url(files/background.jpg);
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  opacity: 0.2;
+`
+
+const StyledHeader = styled.div`
+  display: block;
+  position: relative;
+  background: #32323B;
+  width: 100%;
+  height: 95vh;
+  overflow: hidden;
+  min-height: 40rem;
+
+  &:after {
+    content: ' ';
+    display: block;
+    background: white;
+    width: 130vw;
+    height: 20rem;
+    position: absolute;
+    top: 100%;
+    transform-origin: left bottom;
+    transform: rotateZ(-2deg);
+  }
+`
+
+const Header = () => (
+  <StyledHeader>
+    <StyledHeaderBg />
+  </StyledHeader>
+)
+
+export default Header
